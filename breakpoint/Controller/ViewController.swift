b34ee6c3f2b1f6ada8ef9e8b53978069fcef4742
Artur Z <zarzecki.art@icloud.com>
2021-03-03 17:48:31 +0100

    Init project. Configure with Firebase using cocopods and create TapBar with storyborad

---
+++ breakpoint/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  breakpoint
+//
+//  Created by Artur Zarzecki on 03/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
