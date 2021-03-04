3ef5c0f51a8ef6ec0a1d04e9216b4e249a1f06f3
Artur Z <zarzecki.art@icloud.com>
2021-03-04 10:46:28 +0100

    Set up functions for register and login user Authetication Service

---
+++ breakpoint/Services/AuthService.swift
@@ -0,0 +1,38 @@
+//
+//  AuthService.swift
+//  breakpoint
+//
+//  Created by Artur Zarzecki on 04/03/2021.
+//
+
+import Foundation
+import Firebase
+
+
+class AuthService {
+    static let instance = AuthService()
+    
+    func registerUser(withEmail email: String, andPassword password: String, userCreationComplete: @escaping (_ status: Bool, _ error: Error?) -> ()) { // if is not compleete print error if it is ok it will not call error thats why it is optional
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in // build in function from Firebase
+            guard let user = authResult?.user, error == nil else {
+                userCreationComplete(false, error)
+                return
+            }
+            
+            let userData = ["provider": user.providerID , "email": user.email ]
+            DataService.instance.createDBUser(uid: user.uid, userData: userData)
+            userCreationComplete(true, nil)
+        }
+    }
+    
+    func loginUser(withEmail email: String, andPassword password: String, loginComplete: @escaping (_ status: Bool, _ error: Error?) -> ()) {
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (authResult, error) in
+            guard let user = authResult?.user, error == nil else {
+                loginComplete(false, error)
+                return
+            }
+            loginComplete(true, nil)
+        }
+    }
+}
