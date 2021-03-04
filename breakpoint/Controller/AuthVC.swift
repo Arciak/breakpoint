6a83dddd15d6a07ad1779456bb4ca32036aa0c6d
Artur Z <zarzecki.art@icloud.com>
2021-03-04 09:16:03 +0100

    Set up view for Textedit and setup view for View

---
+++ breakpoint/Controller/AuthVC.swift
@@ -0,0 +1,29 @@
+//
+//  AuthVC.swift
+//  breakpoint
+//
+//  Created by Artur Zarzecki on 04/03/2021.
+//
+
+import UIKit
+
+class AuthVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ breakpoint/View/ShadowView.swift
@@ -0,0 +1,18 @@
+//
+//  ShadowView.swift
+//  breakpoint
+//
+//  Created by Artur Zarzecki on 04/03/2021.
+//
+
+import UIKit
+
+class ShadowView: UIView {
+    
+    override func awakeFromNib() {
+        self.layer.shadowOpacity = 0.75
+        self.layer.shadowRadius = 5
+        self.layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        super.awakeFromNib()
+    }
+}
