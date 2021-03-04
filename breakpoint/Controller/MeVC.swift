5de95e51daf4064cafd42a216d870f5070ea6151
Artur Z <zarzecki.art@icloud.com>
2021-03-04 16:42:29 +0100

    Inititailizxe MeVC

---
+++ breakpoint/Controller/MeVC.swift
@@ -0,0 +1,28 @@
+//
+//  MeVC.swift
+//  breakpoint
+//
+//  Created by Artur Zarzecki on 04/03/2021.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+            
+    }
+    
+    @IBAction func signOutBtnWasPressed(_ sender: Any) {
+        // signout code
+    }
+    
+
+}
