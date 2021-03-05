629ccffc75e9e3c7a96d1a224f2bb1390b74acb6
Artur Z <zarzecki.art@icloud.com>
2021-03-05 11:33:05 +0100

    Create FeedCell

---
+++ breakpoint/View/FeedCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCellTableViewCell.swift
+//  breakpoint
+//
+//  Created by Artur Zarzecki on 05/03/2021.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var contentLbl: UILabel!
+    
+    func configureCell(profileImage: UIImage, email: String, content: String) {
+        self.profileImage.image = profileImage
+        self.emailLbl.text = email
+        self.contentLbl.text = content
+    }
+    
+
+}
