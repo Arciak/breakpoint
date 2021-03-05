a4afe968bacb8d23af326f492d12b6b51a914e46
Artur Z <zarzecki.art@icloud.com>
2021-03-05 12:46:48 +0100

    Getting all feed Messages from firebase and display them into TableView

--- breakpoint/Controller/FeedVC.swift
+++ breakpoint/Controller/FeedVC.swift
@@ -9,21 +9,45 @@ import UIKit
 
 class FeedVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var messageArray = [Message]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) { // to do befor view did loading
+        super.viewDidAppear(animated)
+        DataService.instance.getAllFeedMessages { (returnedMessagesArray) in
+            self.messageArray = returnedMessagesArray
+            self.tableView.reloadData()
+        }
     }
-    */
+    
+}
 
+extension FeedVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messageArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "feedCell") as? FeedCell else { return UITableViewCell() }
+        let image = UIImage(named: "defaultProfileImage")
+        let message = messageArray[indexPath.row]
+        
+        cell.configureCell(profileImage: image!, email: message.senderId, content: message.content)
+        return cell
+    }
+    
+    
 }
